5c5d70e84c65362d47cce9ae4a2b0639526e4994
socamacho <socamachoruiz@gmail.com>
2021-02-26 05:26:07 +0000

    Finished Random Card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //se adiciona a un style
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  //adiciona contenido
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+
+//funcion para obtener numero carta
+let generateRandomNumber = () => {
+  let array = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let random = Math.floor(Math.random() * array.length);
+  return array[random];
+};
+console.log(generateRandomNumber());
+//funcion para obtener tipos carta
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let random2 = Math.floor(Math.random() * suit.length);
+  //generar los valores de manera aleatoria
+
+  return suit[random2];
+};
+console.log(generateRandomSuit());
